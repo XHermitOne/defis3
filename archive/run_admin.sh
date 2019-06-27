0427c2a78ef7ca56847ac6058f5ba7e2754c7ed2
xhermit <xhermitone@gmail.com>
2019-06-27 12:42:30 +0700

    Справочник дилеров. Новая версия

--- archive/run_admin.sh
+++ archive/run_admin.sh
@@ -1,1 +1,1 @@
-/usr/bin/python2 /mnt/defis/defis3/archive/run.py -run -dbg /mnt/defis/defis3/archive/archive/ -s admin
+/usr/bin/python3 /mnt/defis/defis3/ic/engine/run.py -run -dbg /mnt/defis/defis3/archive/archive/ -s admin
