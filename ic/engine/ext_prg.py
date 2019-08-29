3e7f9828f1b33e2e30213c3f9d3936307de8d9a4
xhermit <xhermitone@gmail.com>
2019-08-29 11:18:49 +0700

    Сделан запуск редактора DEFIS из режима выполнения

--- ic/engine/ext_prg.py
+++ ic/engine/ext_prg.py
@@ -4,7 +4,7 @@
 """
 Функции запуска внешних программ.
 Все внешние программы д.б. описаны в словаре
-EXTERNAL_PROGRAMMS_CFG в формате:
+EXTERNAL_PROGRAMS_CFG в формате:
     {
         'имя программмы': {
             'description': 'описание программы',
@@ -23,27 +23,31 @@ import wx
 from ic.log import log
 from ic.utils import system
 
-__version__ = (0, 1, 1, 1)
+__version__ = (0, 1, 2, 1)
 
 
 FILE_PKG_DIRNAME = os.path.dirname(__file__) if os.path.dirname(__file__) else os.getcwd()
-FMSX_DIRNAME = os.path.join(os.path.dirname(FILE_PKG_DIRNAME), 'contrib', 'fMSX')
+# FMSX_DIRNAME = os.path.join(os.path.dirname(FILE_PKG_DIRNAME), 'contrib', 'fMSX')
 IDE_EDITRA_START = os.path.join(os.path.dirname(os.path.dirname(FILE_PKG_DIRNAME)), 'ide', 'Editra-0.7.20', 'editra')
 PYREDITOR_START = os.path.join(os.path.dirname(FILE_PKG_DIRNAME), 'contrib', 'pyreditor', 'pyreditor.pyw')
+DEFIS_START = os.path.join(os.path.dirname(os.path.dirname(FILE_PKG_DIRNAME)), 'editor.sh')
 
-EXTERNAL_PROGRAMMS_CFG = {'calculator': {'description': u'Калькулятор',
-                                         'linux_cmd': 'gnome-calculator&',
-                                         'windows_cmd': None},
-                          'editra': {'description': u'IDE Editra',
-                                     'linux_cmd': '%s %s&' % (sys.executable, IDE_EDITRA_START),
-                                     'windows_cmd': None},
-                          'pyreditor': {'description': u'PyReditor. Редактор регулярных выражений',
-                                        'linux_cmd': '%s %s&' % (sys.executable, PYREDITOR_START),
+EXTERNAL_PROGRAMS_CFG = {'calculator': {'description': u'Калькулятор',
+                                        'linux_cmd': 'gnome-calculator&',
                                         'windows_cmd': None},
-                          # 'kv2': {'description': u'King\'s Valley II MSX game',
-                          #        'linux_cmd': 'cd %s; ./fmsx "./games/kingsvalley2_msx_abandonware.rom"&' % FMSX_DIRNAME,
-                          #        'windows_cmd': None},
-                          }
+                         'editra': {'description': u'IDE Editra',
+                                    'linux_cmd': '%s %s&' % (sys.executable, IDE_EDITRA_START),
+                                    'windows_cmd': None},
+                         'pyreditor': {'description': u'PyReditor. Редактор регулярных выражений',
+                                       'linux_cmd': '%s %s&' % (sys.executable, PYREDITOR_START),
+                                       'windows_cmd': None},
+                         'defis': {'description': u'DEFIS',
+                                   'linux_cmd': 'gnome-terminal -- \'%s\'' % DEFIS_START,
+                                   'windows_cmd': None},
+                         # 'kv2': {'description': u'King\'s Valley II MSX game',
+                         #        'linux_cmd': 'cd %s; ./fmsx "./games/kingsvalley2_msx_abandonware.rom"&' % FMSX_DIRNAME,
+                         #        'windows_cmd': None},
+                         }
 
 
 def run_external_programm(programm_name=None, run_cmd=None):
@@ -62,15 +66,15 @@ def run_external_programm(programm_name=None, run_cmd=None):
         return run_command(run_cmd)
     elif run_cmd is None and programm_name:
         # Команда запуска не определена, но указано имя программы
-        prg = EXTERNAL_PROGRAMMS_CFG.get(programm_name, None)
+        prg = EXTERNAL_PROGRAMS_CFG.get(programm_name, None)
         if prg:
             run_cmd = prg.get('linux_cmd', None) if system.isLinuxPlatform() else prg.get('windows_cmd', None)
             return run_command(run_cmd)
     else:
         # Ничего не определено. Необходимо выбрать программу для запуска
         desc_to_name = dict(
-            [(prg.get('description', u''), prg_name) for prg_name, prg in EXTERNAL_PROGRAMMS_CFG.items()])
-        descriptions = desc_to_name.keys()
+            [(prg.get('description', u''), prg_name) for prg_name, prg in EXTERNAL_PROGRAMS_CFG.items()])
+        descriptions = list(desc_to_name.keys())
         descriptions.sort()
 
         parent_win = wx.GetApp().GetTopWindow()
@@ -84,7 +88,7 @@ def run_external_programm(programm_name=None, run_cmd=None):
                 selected_description = dlg.GetStringSelection()
                 prg_name = desc_to_name.get(selected_description, None)
                 log.debug(u'Выбор <%s> - <%s>' % (selected_description, prg_name))
-                prg = EXTERNAL_PROGRAMMS_CFG.get(prg_name, None)
+                prg = EXTERNAL_PROGRAMS_CFG.get(prg_name, None)
                 if prg:
                     run_cmd = prg.get('linux_cmd', None) if system.isLinuxPlatform() else prg.get('windows_cmd', None)
                     return run_command(run_cmd)
