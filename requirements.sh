ccd16b2b84c641ac73e4fa64a6216690fe9ac329
xhermit <xhermitone@gmail.com>
2019-10-02 11:40:43 +0700

    Замена скриптов развертывания приложений

--- requirements.sh
+++ requirements.sh
@@ -6,96 +6,62 @@
 sudo apt install --assume-yes python3-pip
 
 # Анализаторы кода Python
-apt show pylint3 | grep Package
-apt show pylint3 | grep Version
 sudo apt install --assume-yes pylint3
 
-apt show python3-pep8 | grep Package
-apt show python3-pep8 | grep Version
 sudo apt install --assume-yes python3-pep8
 
 # Модули ОС
-apt show smbfs-utils | grep Package
-apt show smbfs-utils | grep Version
 sudo apt install --assume-yes smbfs-utils
 
-apt show cifs-utils | grep Package
-apt show cifs-utils | grep Version
 sudo apt install --assume-yes cifs-utils
 
-apt show smbclient | grep Package
-apt show smbclient | grep Version
 sudo apt install --assume-yes smbclient
 
-apt show indicator-applet-complete | grep Package
-apt show indicator-applet-complete | grep Version
 sudo apt install --assume-yes indicator-applet-complete
 
-apt show ttf-mscorefonts-installer | grep Package
-apt show ttf-mscorefonts-installer | grep Version
 sudo apt install --assume-yes ttf-mscorefonts-installer
 
-apt show python3-apt | grep Package
-apt show python3-apt | grep Version
 sudo apt install --assume-yes python3-apt
 
 # Работа с консолью
-apt show python3-dialog | grep Package
-apt show python3-dialog | grep Version
 sudo apt install --assume-yes python3-dialog
 
-apt show python3-urwid | grep Package
-apt show python3-urwid | grep Version
 sudo apt install --assume-yes python3-urwid
 
-apt show curl | grep Package
-apt show curl | grep Version
 sudo apt install --assume-yes curl
 
-apt show libsdl1.2debian | grep Package
-apt show libsdl1.2debian | grep Version
 sudo apt install --assume-yes libsdl1.2debian
 
 # wxPython
-# В текущей версии ставиться через pip3
-# apt show python3-wxgtk4.0 | grep Package
-# apt show python3-wxgtk4.0 | grep Version
-# sudo apt install --assume-yes python3-wxgtk4.0
+
+# Установка для Ubuntu 16.04
+# pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04 wxPython
+
 # Обновление:
 # pip3 install wxPython --upgrade
 # Удаление:
 # pip3 uninstall wxPython
-pip3 install -U -function https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-18.04 wxPython
+
+# Установка для Ubuntu 18.04
+pip3 install -U -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-18.04 wxPython
 
 # Поддержка web в wxPython
 sudo apt install --assume-yes libwebkitgtk-3.0-0
 
 pip3 install objectlistview
 
-apt show python3-six | grep Package
-apt show python3-six | grep Version
 sudo apt install --assume-yes python3-six
 
-apt show python3-matplotlib | grep Package
-apt show python3-matplotlib | grep Version
 sudo apt install --assume-yes python3-matplotlib
 
 # Нет в репозитариях
-# apt show python3-wxmpl | grep Package
-# apt show python3-wxmpl | grep Version
 # sudo apt install --assume-yes python3-wxmpl
 
 # БД
-apt show python3-psycopg2 | grep Package
-apt show python3-psycopg2 | grep Version
 sudo apt install --assume-yes python3-psycopg2
 
-apt show python3-sqlalchemy | grep Package
-apt show python3-sqlalchemy | grep Version
 sudo apt install --assume-yes python3-sqlalchemy
 
-apt show python3-pyodbc | grep Package
-apt show python3-pyodbc | grep Version
 sudo apt install --assume-yes unixodbc unixodbc-dev freetds-bin freetds-dev tdsodbc python3-pyodbc
 
 # pip3 install dbfpy
@@ -106,28 +72,16 @@ pip3 install JayDeBeApi
 pip3 install dbfread
 
 # Office
-apt show unococnv | grep Package
-apt show unococnv | grep Version
 sudo apt install --assume-yes unoconv
 
-apt show python3-sane | grep Package
-apt show python3-sane | grep Version
 sudo apt install --assume-yes python3-sane
 
-apt show python3-reportlab | grep Package
-apt show python3-reportlab | grep Version
 sudo apt install --assume-yes python3-reportlab
 
-apt show python3-pypdf2 | grep Package
-apt show python3-pypdf2 | grep Version
 sudo apt install --assume-yes python3-pypdf2
 
-apt show python3-odf | grep Package
-apt show python3-odf | grep Version
 sudo apt install --assume-yes python3-odf python-odf-doc
 
-apt show libreoffice-java-common | grep Package
-apt show libreoffice-java-common | grep Version
 sudo apt install --assume-yes libreoffice-java-common
 
 sudo apt --fix-broken install --assume-yes
